2e3b4351f62ca6da7dc9d61cf7440684863d88a7
Thaumy <thaumy@outlook.com>
2023-09-16 09:47:32 +0800

    fix: dup img by symlink

--- src/find_img.rs
+++ src/find_img.rs
@@ -2,11 +2,9 @@ use std::collections::HashSet;
 use std::ffi::{OsStr, OsString};
 use std::fs;
 use std::path::Path;
-use std::sync::Arc;
 
 use anyhow::Result;
 use colored::Colorize;
-use crossbeam::queue::SegQueue;
 
 #[inline]
 fn is_img_ext(ext: OsString) -> bool {
@@ -18,24 +16,38 @@ fn is_img_ext(ext: OsString) -> bool {
 }
 
 pub fn find_img(
-    img_paths: &Arc<SegQueue<String>>,
+    img_paths: &mut HashSet<String>,
     root_path: &Path,
     ignore_paths: &HashSet<String>
 ) -> Result<()> {
+    // dedup
     if ignore_paths.contains(root_path.to_str().unwrap()) {
+        return Ok(());
+    }
+    // ignore symlink
+    if root_path.is_symlink() {
         return Ok(());
     }
 
     for entry in fs::read_dir(root_path)? {
         let path = entry?.path();
+        // ignore symlink
+        if path.is_symlink() {
+            continue;
+        }
 
         if path
             .extension()
             .map(|ext| ext.to_ascii_lowercase())
             .is_some_and(is_img_ext)
         {
-            println!("{} {}", "[PATH]".yellow(), path.display());
-            img_paths.push(format!("{}", path.display()))
+            println!(
+                "{} {} {}",
+                "[PATH]".yellow(),
+                img_paths.len(),
+                path.display()
+            );
+            img_paths.insert(format!("{}", path.display()));
         } else if path.is_dir() {
             find_img(img_paths, path.as_path(), ignore_paths)?;
         }
