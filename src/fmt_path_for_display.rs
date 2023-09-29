04e9fe22c37300ae41afc0d3d191179cef436ab4
Thaumy <thaumy@outlook.com>
2023-09-29 12:56:53 +0800

    refactor: use terminal_size

--- src/fmt_path_for_display.rs
+++ src/fmt_path_for_display.rs
@@ -1,5 +1,8 @@
+use terminal_size::Width;
+
 pub fn fmt_path_for_display(path: &str, prefix_len: usize) -> String {
-    let diff = term_size::dimensions().unwrap().0 as i32 - path.len() as i32 - prefix_len as i32;
+    let (Width(width), _) = terminal_size::terminal_size().expect("Can not get terminal size");
+    let diff = width as i32 - path.len() as i32 - prefix_len as i32;
 
     if diff >= 0 {
         path.to_owned()
