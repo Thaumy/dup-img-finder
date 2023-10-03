e74d4048f48d4ceca5a67f28e9ffce0235f06bd0
Thaumy <thaumy@outlook.com>
2023-10-03 13:50:12 +0800

    feat: default terminal width

--- src/fmt_path_for_display.rs
+++ src/fmt_path_for_display.rs
@@ -1,12 +1,15 @@
 use terminal_size::Width;
 
 pub fn fmt_path_for_display(path: &str, prefix_len: usize) -> String {
-    let (Width(width), _) = terminal_size::terminal_size().expect("Can not get terminal size");
+    let width = terminal_size::terminal_size()
+        .map(|(Width(w), _)| w)
+        .unwrap_or(128);
     let diff = width as i32 - path.len() as i32 - prefix_len as i32;
 
     if diff >= 0 {
         path.to_owned()
     } else {
-        path.chars().skip(prefix_len - diff as usize).collect()
+        let skip = prefix_len.saturating_sub(diff as usize);
+        path.chars().skip(skip).collect()
     }
 }
