fe2b50aa3a9db739f9924c355727e7cc33c20743
Thaumy <thaumy@outlook.com>
2023-10-03 13:50:54 +0800

    refactor: lift out image open

--- src/read_file.rs
+++ src/read_file.rs
@@ -1,11 +1,13 @@
+use anyhow::Result;
 use colored::Colorize;
-use image::{DynamicImage, ImageResult};
+use std::fs;
 
 use crate::fmt_path_for_display::fmt_path_for_display;
 
-#[inline]
-pub fn read_img(percent: usize, img_path: &str) -> ImageResult<DynamicImage> {
+pub fn read_file(percent: usize, img_path: &str) -> Result<Vec<u8>> {
     let display_path = fmt_path_for_display(img_path, 12);
     println!("{} {:>3}% {}", "[READ]".blue(), percent, display_path);
-    image::open(img_path)
+
+    let bytes = fs::read(img_path)?;
+    Ok(bytes)
 }
