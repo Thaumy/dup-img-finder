b8f300bf94d13b7923964a64099083ff62a5e8d6
Thaumy <thaumy@outlook.com>
2023-10-03 13:51:07 +0800

    refactor: lift out hasher

--- src/calc_img_hash.rs
+++ src/calc_img_hash.rs
@@ -1,22 +1,24 @@
 use std::sync::mpsc::Sender;
 
+use anyhow::Result;
 use colored::Colorize;
-use image_hasher::HasherConfig;
+use image::DynamicImage;
+use image_hasher::Hasher;
 
 use crate::fmt_path_for_display::fmt_path_for_display;
-use crate::read_img::read_img;
+use crate::read_file::read_file;
 
-#[inline]
 #[allow(clippy::type_complexity)]
 pub fn calc_img_hash(
     percent: usize,
+    hasher: &Hasher,
     img_path: String,
     img_hash_result_tx: &Sender<Result<(Box<[u8]>, String), String>>,
 ) {
-    let hasher = HasherConfig::new().to_hasher();
-
     let display_path = fmt_path_for_display(&img_path, 12);
-    let result = match read_img(percent, &img_path) {
+    let img_data: Result<DynamicImage> =
+        try { image::load_from_memory(&read_file(percent, &img_path)?[..])? };
+    let result = match img_data {
         Ok(img) => {
             println!("{} {:>3}% {}", "[CALC]".cyan(), percent, display_path);
             let hash = Box::from(hasher.hash_image(&img).as_bytes());
