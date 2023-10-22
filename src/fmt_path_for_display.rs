260d8980cab43ba18388cdf71485f3b330803c62
Thaumy <thaumy@outlook.com>
2023-10-23 00:40:56 +0800

    feat: clip path for term size

--- src/fmt_path_for_display.rs
+++ src/fmt_path_for_display.rs
@@ -1,15 +1,16 @@
 use terminal_size::Width;
+use unicode_width::UnicodeWidthStr;
 
 pub fn fmt_path_for_display(path: &str, prefix_len: usize) -> String {
     let width = terminal_size::terminal_size()
         .map(|(Width(w), _)| w)
         .unwrap_or(128);
-    let diff = width as i32 - path.len() as i32 - prefix_len as i32;
+    let diff = width as i32 - path.width_cjk() as i32 - prefix_len as i32;
 
     if diff >= 0 {
         path.to_owned()
     } else {
-        let skip = prefix_len.saturating_sub(diff as usize);
+        let skip = -diff as usize;
         path.chars().skip(skip).collect()
     }
 }
